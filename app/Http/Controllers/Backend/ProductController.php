312ba5378b6fcf87b73f69e66a956ba271f5da6c
Yeasin123 <yeasin15-1037@diu.edu.bd>
2021-09-11 14:15:43 +0600

    add category,brand,product

--- app/Http/Controllers/Backend/ProductController.php
+++ app/Http/Controllers/Backend/ProductController.php
@@ -7,6 +7,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Validator;
 use Illuminate\Support\Str;
 use App\Models\Product;
+use App\Models\ProductImage;
 
 class ProductController extends Controller
 {
@@ -31,21 +32,34 @@ class ProductController extends Controller
             $Product = new Product;
             $Product->name = $request->name;
             $Product->category_id = $request->category_id;
+            $Product->brand_id = $request->brand_id;
             $Product->stock = $request->stock;
             $Product->model = $request->model;
             $Product->color = $request->color;
             $Product->price = $request->price;
+            $Product->year = $request->year;
             $Product->description = $request->description;
             $Product->status = true;
             $Product->slug = Str::slug($Product->name);
-
-            $Product->status = true;
-
-            $imageName = rand().'.'.$request->image->getClientOriginalExtension();
-    
-            $request->image->move(public_path('/images/product'), $imageName);
-            $Product->image = $imageName;
             $Product->save();
+
+            if ( $request->hasfile('product_image'))
+                {
+                    $files = $request->file('product_image');
+                    foreach( $files  as $image )
+                    {
+                        $img = rand() . '.' . $image->getClientOriginalExtension();
+                        $image->move(public_path('/images/product'), $img);
+                        $p_image = new ProductImage;
+                        $p_image->product_id = $Product->id;
+                        $p_image->product_image = $img;
+                        $p_image->save();
+                    }
+        
+                }
+                else{
+
+                }
             return response()->json(['message' => 'Product Added Successfully'],200);
             
         }
@@ -78,16 +92,21 @@ class ProductController extends Controller
             $Product->slug = Str::slug($Product->name);
             $Product->status = true;
 
-            if($request->hasFile('image')){
+            if($request->hasFile('product_image')){
                 unlink(public_path('/images/product/'.$Product->image));
 
-                $imageName = rand().'.'.$request->image->getClientOriginalExtension();
-                $request->image->move(public_path('/images/product'), $imageName);
-                $Product->image = $imageName;
-
-           }else{
-            $Product->image =$Product->image;
+                $files = $request->file('product_image');
+                    foreach( $files  as $image )
+                    {
+                        $img = rand() . '.' . $image->getClientOriginalExtension();
+                        $image->move(public_path('/images/product'), $img);
+                        $p_image = new ProductImage;
+                        $p_image->product_id = $Product->id;
+                        $p_image->product_image = $img;
+                        $p_image->save();
+                    }
            }
+
             $Product->update();
             return response()->json(['message' => 'Product Updated Successfully'],200);
            
@@ -104,8 +123,13 @@ class ProductController extends Controller
         $Product = Product::find($id);
 
         if($Product){
-            
-            unlink(public_path('/images/product/'.$Product->image));
+
+            $productimaged = ProductImage::where('product_id',  $Product->id)->get();
+            foreach ($productimaged as $productimageId) {
+                // unlink(public_path('/images/product/'.$Product->image));
+                $productimageId = ProductImage::find($productimageId->id);
+                $productimageId->delete();
+            }
             $Product->delete();
             return response()->json('Product Delete Successfully');
         }
--- database/migrations/2021_09_11_050542_create_products_table.php
+++ database/migrations/2021_09_11_050542_create_products_table.php
@@ -17,16 +17,17 @@ class CreateProductsTable extends Migration
             $table->id();
             $table->string('name')->nullable();
             $table->unsignedBigInteger('category_id');
+            $table->unsignedBigInteger('brand_id');
             $table->integer('stock')->nullable();
             $table->string('model')->nullable();
             $table->string('color')->nullable();
-            $table->string('image')->nullable();
             $table->string('price')->nullable();
             $table->string('year')->nullable();
             $table->text('description')->nullable();
             $table->string('slug');
             $table->boolean('status')->default(true);
             $table->foreign('category_id')->references('id')->on('categories')->onDelete('cascade');
+            $table->foreign('brand_id')->references('id')->on('brands')->onDelete('cascade');
             $table->timestamps();
         });
     }
